920462a46d70587b48bbcc50169a1a3632876f48
Jack Yeh <jack.yeh@YehJ.local>
2016-11-23 18:30:41 -0700

    fill out the components and wire it up to be displayed in reduxtagram.js

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -8,6 +8,7 @@ const Main = React.createClass({
         <h1>
           <Link to="/">TinderCariously Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     )
   }
